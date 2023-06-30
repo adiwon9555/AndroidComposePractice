57483a9bacd1dc4b771386aaffe54b5d81ca424d
adiwon <aditya.rathi@vedantu.com>
2023-06-30 18:01:14 +0530

    happy bday card done

--- app/src/main/java/com/example/androidplaygroundcomponse/birthdayCardUI/BirthayCardActivity.kt
+++ app/src/main/java/com/example/androidplaygroundcomponse/birthdayCardUI/BirthayCardActivity.kt
@@ -3,13 +3,17 @@ package com.example.androidplaygroundcomponse.birthdayCardUI
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidplaygroundcomponse.birthdayCardUI.ui.theme.BirthdayCardTheme
 
 class BirthayCardActivity : ComponentActivity() {
@@ -22,22 +26,34 @@ class BirthayCardActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting2("Android")
+                    Greeting2(text = "Happy Birthday Aditya",from = "Don")
                 }
             }
         }
     }
 }
-
+//@Composable function name must be in Pascal case(All words capitalized)
+//Describes some part of your UI.
+//Doesn't return anything.
 @Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting2(text: String, from : String,modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(text = "$text", fontSize = 100.sp, lineHeight = 116.sp, textAlign = TextAlign.Center)
+        Text(text = "from $from",fontSize = 36.sp, modifier = Modifier.padding(16.dp).align(alignment = Alignment.End))
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme() {
-        Greeting2("Android")
+        Greeting2(text = "Happy Birthday Aditya",from = "Don")
     }
 }
